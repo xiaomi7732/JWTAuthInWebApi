79d4d9b2be3163bbe2669e197d2d1547145b2a62
Saar Shen <xiaomi7732@hotmail.com>
2022-05-30 09:08:59 -0700

    Update the cotroller to accept multiple roles

--- Controllers/ProtectedResourceController.cs
+++ Controllers/ProtectedResourceController.cs
@@ -3,11 +3,14 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace JWT
 {
+    /// <summary>
+    /// Controller that allows multiple roles to access.
+    /// </summary>
     public class ProtectedResourceController : ControllerBase
     {
         [Route("protectedInfo")]
         [HttpGet]
-        [Authorize(Roles = "User")]
+        [Authorize(Roles = "User, Admin")]
         public IActionResult Get()
         {
             return Ok("You can see this message means you are a valid user.");
